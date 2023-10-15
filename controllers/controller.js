3cf067a4d7219386d69f7193e24abf9eb30c91e0
nikhil03a <anikhil03prasanna@gmail.com>
2023-10-15 19:58:22 +0530

    Score Updation

--- controllers/controller.js
+++ controllers/controller.js
@@ -96,8 +96,8 @@ const updateArray = (array, id, score) => {
     array = array.map((item) => {
         if (item.id === id) {
             let oldScore = item.score;
-            let newScore = oldScore+score;
-            return { ...item, score: newScore }; // Update the score for the specific item
+            let newScore = oldScore + score;
+            return { ...item, score: newScore };
         }
         return item;
     });
@@ -106,57 +106,78 @@ const updateArray = (array, id, score) => {
 const calculateScore = async (req, res) => {
     try {
         const match = await Match.findOne({ id: req.params.id })
-        let players = []
+        let players = [];
         players.push(...match.battingTeam1);
         players.push(...match.battingTeam2);
         players.push(...match.bowlingTeam1);
         players.push(...match.bowlingTeam2);
-        let score = []
-        players.map((player) => {
+        let playerScores = {};
+        players.forEach((player) => {
             const id = player.id;
-            if (!(id in score)) {
-                score = [...score, { id: player.id, score:0}]
+            if (!playerScores[id]) {
+                playerScores[id] = 0;
             }
-            if ("runs" in player) {
-                score = updateArray(score,id,player.runs)
-                score = updateArray(score,id,Math.floor(player.runs/50)*4)
-                score = updateArray(score,id,Math.floor(player.runs/100)*8)
-                if(player.runs == 0) score = updateArray(score,id,-3)
+            if (player.runs !== undefined) {
+                playerScores[id] += player.runs;
+                playerScores[id] += Math.floor(player.runs / 50) * 4;
+                playerScores[id] += Math.floor(player.runs / 100) * 8;
+                if (player.runs === 0) {
+                    playerScores[id] -= 3;
+                }
             }
-            if ("fours" in player){
-                score = updateArray(score,id,player.fours)
+            if ("wickets" in player) {
+                playerScores[id] += player.wickets * 25;
+                if (player.wickets >= 3) {
+                    playerScores[id] += Math.floor(player.wickets / 3) * 4;
+                }
+                if (player.wickets >= 4) {
+                    playerScores[id] += Math.floor(player.wickets / 4) * 8;
+                }
             }
-            if("sixes" in player){
-                score = updateArray(score,id,player.sixes*3)
+            if ("maidens" in player) {
+                playerScores[id] += player.maidens * 4;
             }
-            if("wickets" in player){
-                score = updateArray(score,id,player.wickets*25)
-                score = updateArray(score,id,Math.floor(player.wickets/3)*4)
-                score = updateArray(score,id,Math.floor(player.wickets/4)*8)
+            if (player.strikeRate !== undefined) {
+                const sr = player.strikeRate;
+                if (sr > 140) playerScores[id] += 6;
+                else if (sr > 120) playerScores[id] += 4;
+                else if (sr > 100) playerScores[id] += 2;
+                else if (sr > 40) playerScores[id] -= 2;
+                else if (sr > 30) playerScores[id] -= 4;
+                else playerScores[id] -= 6;
             }
-            if("maidens" in player){
-                score = updateArray(score,id,player.maidens*4)
+            if ("economy" in player) {
+                const eco = player.economy;
+                if (eco < 2.5) playerScores[id] += 6;
+                else if (eco < 3.5) playerScores[id] += 4;
+                else if (eco < 4.5) playerScores[id] += 2;
+                else if (eco < 8) playerScores[id] -= 2;
+                else if (eco < 9) playerScores[id] -= 4;
+                else playerScores[id] -= 6;
             }
-            if("strikeRate" in player){
-                let sr = player.strikeRate;
-                if(sr > 140) score = updateArray(score,id,6)
-                else if(sr > 120) score = updateArray(score,id,4)
-                else if(sr > 100) score = updateArray(score,id,2)
-                else if(sr > 40) score = updateArray(score,id,-2)
-                else if(sr > 30) score = updateArray(score,id,-4)
-                else score = updateArray(score,id,-6)
-            }
-            if("economy" in player){
-                let eco = player.economy;
-                if(eco < 2.5) score = updateArray(score,id,6)
-                else if(eco < 3.5) score = updateArray(score,id,4)
-                else if(eco < 4.5) score = updateArray(score,id,2)
-                else if(eco < 8) score = updateArray(score,id,-2)
-                else if(eco < 9) score = updateArray(score,id,-4)
-                else score = updateArray(score,id,-6)
-            }
+        });
+
+        const users = await Selections.find({ mid: req.params.id });
+        users.map((user) => {
+            let userScore = 0;
+            user.selection.map((player) => {
+                if(playerScores[player.id] !== undefined){
+                    if(player.playerRole === "captain"){
+                        userScore += 2*playerScores[player.id];
+                    }else if(player.playerRole === "viceCaptain"){
+                        userScore += 1.5*playerScores[player.id];
+                    }else{
+                        userScore += playerScores[player.id];
+                    }
+                }
+            })
+            Users.updateOne({ id: user.id }, { $inc: { score: userScore } }, (err, result) => {
+                if (err) {
+                    console.log(err);
+                }
+            })
         })
-        res.status(201).json({ data: score });
+        res.status(201).json({message: "Successfully Updated Score" })
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
