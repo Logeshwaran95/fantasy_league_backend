49babac6932cdb5164edc1616926627c7b816abe
nikhil03a <anikhil03prasanna@gmail.com>
2024-03-11 07:10:43 +0530

    changes

--- index.js
+++ index.js
@@ -26,7 +26,12 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended:true, limit: '50mb'}));
 app.use(morgan('dev'));
 app.use(cors());
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://fantasyleague.cseaceg.org.in');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => {
     console.log(`Server started listening on ${PORT}`);
